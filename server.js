78db24e0be81f04a3279a7018f2b6e8d39111595
mrTulsyan <maheep2403@gmail.com>
2024-07-21 16:47:04 +0530

    Made some changes

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import csvUploadRoutes from './routes/csvUpload.js';
 import getDetailsRoutes from './routes/getDetails.js';
 
 // Import the database connection
 import connectDB from './database/database.js';
+
+//cors
+const corsConfig = {
+    origin: "*",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+};
+app.options("", cors(corsConfig));
+app.use(cors(corsConfig));
+
 
 dotenv.config({
     path: './.env',
